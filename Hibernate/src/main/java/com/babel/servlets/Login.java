593461136e432417ac57ad2e77ee8921e8ad38cc
andra01 <andra.vancea@gmail.com>
2016-07-13 18:55:57 +0300

    Add files via upload

---
+++ Hibernate/src/main/java/com/babel/servlets/Login.java
@@ -0,0 +1,40 @@
+package com.babel.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Login extends HttpServlet {
+
+	/**
+	 * Default serial id
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		req.getRequestDispatcher("/jsp/login.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// Login logic
+		String firstName = req.getParameter("firstname").toString();
+		String password = req.getParameter("password").toString();
+
+		if (!"admin".equals(firstName) || !"pass".equals(password)) {
+			// not correct
+			// go back to login
+			doGet(req, resp);
+		} else {
+			// correct! Greet!
+			req.setAttribute("firstname", firstName);
+			req.getRequestDispatcher("/Greeting").forward(req, resp);
+		}
+	}
+
+}
---
+++ Hibernate/src/main/webapp/jsp/Greeting.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! <%= (String)request.getAttribute("firstname") %></h2>
+</body>
+</html>
